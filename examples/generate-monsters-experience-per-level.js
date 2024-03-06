8feac46209d677d1af94371c76b55f105a3af34e
Damian A. Pastorini <damian.pastorini@gmail.com>
2024-03-06 19:54:08 +0100

    - Reldens - Game Data Generator

---
+++ examples/generate-monsters-experience-per-level.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * Reldens - Game Data Generator - Examples
+ *
+ */
+
+const { MonstersExperiencePerLevel } = require('../lib/generator/monsters-experience-per-level');
+const playerLevels = require('./players-experience-per-level.json');
+const { Logger } = require('@reldens/utils');
+
+const monstersExperiencePerLevel = new MonstersExperiencePerLevel({
+    levelsExperienceByKey: playerLevels,
+    variationsMinMax: {
+        monsterA: {min: 0.07, max: 0.10}, // 7% - 10%
+        monsterB: {min: 0.10, max: 0.13}, // 10% - 13%
+        bossExtra: {min: 0, max: 0.5} // 0% - 50%
+    },
+    everyLevelsQuantity: 5,
+
+});
+
+monstersExperiencePerLevel.generate().catch((error) => {
+    Logger.error(error);
+}).then(() => {
+    Logger.info('Data saved! Check the "generated" folder.');
+});
